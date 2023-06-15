05e4524b8de83016dcc1569a9e1f992961a103a6
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-15 22:28:11 +0100

    db config

---
+++ server/config/database.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.PORT),
+  dialect: 'postgres',
+});
+
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testDatabaseConnection();
+
+export default sequelize;
