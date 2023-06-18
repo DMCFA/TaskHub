26bc9078689af50f9afb9aee2cabeb404d81d85f
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-18 22:22:16 +0100

    add get single user and login route + tokens

--- server/app/controllers/users.ts
+++ server/app/controllers/users.ts
@@ -2,6 +2,8 @@ import bcrypt from 'bcrypt';
 import { Op } from 'sequelize';
 import User from '../models/User';
 import { Request, Response } from 'express';
+import { generateToken } from '../../utils/tokenUtils';
+import { UserAttributes, UserInstance } from '../../types/User';
 
 //GET api/users
 //Description: Get all users
@@ -15,7 +17,27 @@ export const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
-//POST api/user
+//GET api/users/[id]
+//Description: Get user details
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    return res.json(user);
+  } catch (error) {
+    console.error('Error retrieving user information:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+//POST api/users/register
 //Description: Register users
 export const registerUser = async (req: Request, res: Response) => {
   try {
@@ -39,17 +61,56 @@ export const registerUser = async (req: Request, res: Response) => {
 
     //create user
     const newUser = await User.create({
-      username: username,
+      username,
       fname,
       email,
       password: hashedPassword,
       created_on: new Date(),
       last_login: null,
-    });
+    } as UserAttributes);
+
+    const userInstance = newUser as UserInstance;
+
+    //generate session token
+    const { user_id } = userInstance;
+    const token = generateToken(user_id);
+
+    //update login time
+    await newUser.update({ last_login: new Date() });
 
     return res.status(201).json(newUser);
   } catch (error) {
     console.error('Error in user registration', error);
     return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+//POST api/users/login
+//Description: Login users
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username or email
+    const user = await User.findOne({
+      where: { [Op.or]: [{ username }, { email: username }] },
+    });
+
+    // User not found/password mismatch
+    if (!user || !(await user.isValidPassword(password))) {
+      return res
+        .status(401)
+        .json({ error: 'Invalid username/email or password' });
+    }
+
+    // Generate session token
+    const token = generateToken(user.user_id);
+
+    // Update last login timestamp
+    await user.update({ last_login: new Date() });
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error logging in' });
   }
 };
