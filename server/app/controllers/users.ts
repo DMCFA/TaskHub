50e4cb3824281bfdc3eb426c50c9ca70c5c033b2
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-21 18:34:25 +0100

    update user controller

--- server/app/controllers/users.ts
+++ server/app/controllers/users.ts
@@ -78,7 +78,7 @@ export const registerUser = async (req: Request, res: Response) => {
     //update login time
     await newUser.update({ last_login: new Date() });
 
-    res.cookie('auth_token', token, {
+    res.cookie('access_token', token, {
       expires: sessionExpirationDate(),
       httpOnly: true,
     });
@@ -115,7 +115,7 @@ export const loginUser = async (req: Request, res: Response) => {
     await user.update({ last_login: new Date() });
 
     //add session cookie
-    res.cookie('auth_token', token, {
+    res.cookie('access_token', token, {
       expires: sessionExpirationDate(),
       httpOnly: true,
     });
@@ -123,5 +123,35 @@ export const loginUser = async (req: Request, res: Response) => {
     res.status(200).json({ user, token });
   } catch (error) {
     res.status(500).json({ error: 'Error logging in' });
+  }
+};
+
+//PUT api/users/:id
+//Description: Update user properties
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { username, email, fname, password } = req.body;
+
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    //update user fields
+    user.username = username ?? user.username;
+    user.fname = fname ?? user.fname;
+    user.email = email ?? user.email;
+
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
+    }
+
+    await user.save();
+
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    console.error('Error updating user', error);
+    res.status(500).json({ error: 'Error updating user' });
   }
 };
