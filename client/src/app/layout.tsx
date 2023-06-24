7a6a73e68f43f1c99f1f37c8c814310180c91da0
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-24 21:46:27 +0100

    clean root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,10 +3,7 @@
 import './styles.scss';
 import { Roboto } from 'next/font/google';
 import { CssBaseline } from '@mui/material';
-import Navbar from './components/Nav/Navbar';
 import { CustomThemeProvider } from '../utils/ThemeContext';
-import { user } from '../store/mock';
-import Welcome from './pages/welcome';
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
@@ -28,18 +25,7 @@ export default function RootLayout({
     <CustomThemeProvider>
       <CssBaseline />
       <html lang='en'>
-        {user ? (
-          <body className={roboto.className}>
-            <header>
-              <Navbar />
-            </header>
-            {children}
-          </body>
-        ) : (
-          <body className={roboto.className}>
-            <Welcome />
-          </body>
-        )}
+        <body className={roboto.className}>{children}</body>
       </html>
     </CustomThemeProvider>
   );
