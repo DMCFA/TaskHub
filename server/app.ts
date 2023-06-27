14d905de760a6cf7c5f9fcf85f2ce6b0a2688472
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-27 10:09:23 +0100

    fix cors

--- server/app.ts
+++ server/app.ts
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3003;
 
 //middlewares
-app.use(cors);
+app.use(cors());
 app.use('/api', limiter);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
