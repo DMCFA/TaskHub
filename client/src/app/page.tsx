2cd088f0d31535dea4e192e26099fab908fc3032
Duarte Almeida <duartemalmeida@gmail.com>
2023-06-29 19:49:20 +0100

    implement getting cookie and conditional rendering

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { cookies } from 'next/headers';
+import Dashboard from './home/page';
+import Welcome from './components/Welcome';
+
 export default function Home() {
-  return (
-    <main>
-      <h1>TaskHub</h1>
-    </main>
-  );
+  const cookieStore = cookies();
+  const accessCookie = cookieStore.get('access_token');
+
+  return <>{accessCookie ? <Dashboard /> : <Welcome />}</>;
 }
