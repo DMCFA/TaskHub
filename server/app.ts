f52a4e1cc8290e7345b2d1e6c9b84a927dd69d91
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-01 16:00:20 +0100

    add cookie parser

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import { limiter, corsOptions } from './config/express';
 import userRouter from './app/routes/users';
 import { globalErrorHandler } from './app/middleware/middleware';
@@ -13,6 +14,7 @@ const port = process.env.PORT || 3003;
 //middlewares
 app.use(cors(corsOptions));
 app.use('/api', limiter);
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(globalErrorHandler);
