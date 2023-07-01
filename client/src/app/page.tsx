fcd8c072be3135d73c0258bc524114f41dfdcebb
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-01 16:46:11 +0100

    implement persistent login logic and redirects

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,46 @@
-import { cookies } from 'next/headers';
-import Dashboard from './home/page';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Welcome from './components/Welcome';
+import HomeLoader from './components/HomeLoader';
+import { loginSuccess } from '../services/features/userSlice';
 
 export default function Home() {
-  const cookieStore = cookies();
-  const accessCookie = cookieStore.get('access_token');
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
-  return <>{accessCookie ? <Dashboard /> : <Welcome />}</>;
+  useEffect(() => {
+    async function autoLogin() {
+      try {
+        const response = await fetch(
+          'http://localhost:3001/api/users/newuser-auth',
+          {
+            method: 'POST',
+            credentials: 'include',
+          }
+        );
+        if (response.status === 200) {
+          const userResponse = await response.json();
+          const user = userResponse.user;
+          dispatch(loginSuccess(user));
+          router.push('/dashboard');
+        } else {
+          console.error('Auto-login failed:', response.status);
+        }
+      } catch (error) {
+        console.error('Error trying to authenticate', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    autoLogin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className='landing'>{isLoading ? <HomeLoader /> : <Welcome />}</div>
+  );
 }
