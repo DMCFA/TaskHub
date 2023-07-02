4f457b7b8b90be0061511021fe3806c1345f756c
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-02 22:37:53 +0100

    add redux, create store and user slice

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,8 +4,13 @@ import './styles.scss';
 import { Roboto } from 'next/font/google';
 import { CssBaseline } from '@mui/material';
 import { CustomThemeProvider } from '../utils/ThemeContext';
+import { Provider } from 'react-redux';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { store } from './store';
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
+
+const queryClient = new QueryClient();
 
 export const metadata = {
   title: 'TaskHub',
@@ -22,11 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <CustomThemeProvider>
-      <CssBaseline />
-      <html lang='en'>
-        <body className={roboto.className}>{children}</body>
-      </html>
-    </CustomThemeProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <CustomThemeProvider>
+          <CssBaseline />
+          <html lang='en'>
+            <body className={roboto.className}>{children}</body>
+          </html>
+        </CustomThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
---
+++ client/src/services/features/userSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+interface User {
+  user_id: number;
+  username: string;
+  fname: string;
+  email: string;
+  created_on: Date;
+  last_login: Date | null;
+  is_admin: boolean;
+}
+
+interface UserState {
+  isActive: boolean;
+  user: User | null;
+}
+
+const initialState: UserState = {
+  isActive: false,
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginSuccess: (state, action: PayloadAction<User>) => {
+      (state.isActive = true), (state.user = action.payload);
+    },
+    logout: (state, action: PayloadAction<User>) => {
+      (state.isActive = false), (state.user = null);
+    },
+  },
+});
+
+export const { loginSuccess, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user;
+
+export default userSlice.reducer;
