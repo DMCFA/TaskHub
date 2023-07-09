9d8d7b810a6ac6d860b96f5899f5a7e595359948
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-09 18:55:27 +0100

    registerUser route

--- client/src/app/pages/api/users.ts
+++ client/src/app/pages/api/users.ts
@@ -24,3 +24,28 @@ export const loginUser = async (data: {
     console.error('Error:', error);
   }
 };
+
+export const registerUser = async (data: {
+  username: string;
+  fname: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const res = await fetch(`${baseUrl}/register`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+      credentials: 'include',
+    });
+
+    if (res.ok) {
+      const { newUser: user } = await res.json();
+      return user;
+    } else {
+      throw new Error('Failed to register user');
+    }
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
