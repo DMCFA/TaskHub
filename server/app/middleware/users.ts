18d915af773bf761157a78fbb5fae529ea086154
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-18 18:49:23 +0100

    add checkAdmin middleware

--- server/app/middleware/users.ts
+++ server/app/middleware/users.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import User from '../models/User';
 import { verifyToken } from '../../utils/tokenUtils';
 import { UserInstance } from '../../types/User';
 
@@ -29,5 +30,30 @@ export const authenticateUser = (
   } catch (error) {
     console.error('Error verifying token', error);
     return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+export const checkAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userId = req.user?.user_id;
+
+  try {
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized access' });
+    }
+
+    if (!user.is_admin) {
+      return res.status(403).json({ error: 'Access denied' });
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error checking admin:', error);
+    res.status(500).json({ error: 'Failed to check admin status' });
   }
 };
