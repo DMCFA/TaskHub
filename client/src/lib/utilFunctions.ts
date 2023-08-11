3530a26ce34470e36e60503df5b6f125429d5126
Duarte Almeida <duartemalmeida@gmail.com>
2023-08-11 21:50:20 +0100

    add getTime function

--- client/src/lib/utilFunctions.ts
+++ client/src/lib/utilFunctions.ts
@@ -18,3 +18,40 @@ export const shuffleArray = <T>(array: T[]): T[] => {
     .sort((a, b) => a.sort - b.sort)
     .map((item) => item.value);
 };
+
+export const getTimeLeft = (dueDate: Date | string): string => {
+  if (!dueDate) return '-';
+
+  if (typeof dueDate === 'string') {
+    dueDate = new Date(dueDate);
+  }
+  if (!(dueDate instanceof Date)) {
+    console.error('Invalid date provided:', dueDate);
+    return '-';
+  }
+
+  const currentDate = new Date();
+
+  let difference = dueDate.getTime() - currentDate.getTime();
+
+  if (difference <= 0) {
+    return 'Overdue';
+  }
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  difference -= days * (1000 * 60 * 60 * 24);
+
+  const hours = Math.floor(difference / (1000 * 60 * 60));
+  difference -= hours * (1000 * 60 * 60);
+
+  if (days > 30) {
+    const months = Math.floor(days / 30);
+    return `${months} month${months > 1 ? 's' : ''} left`;
+  } else if (days > 0) {
+    return `${days} day${days > 1 ? 's' : ''} ${hours} hour${
+      hours > 1 ? 's' : ''
+    } left`;
+  } else {
+    return `${hours} hour${hours > 1 ? 's' : ''} left`;
+  }
+};
